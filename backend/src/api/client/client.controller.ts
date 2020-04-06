77eca526d8322a058c8395d9b72fc27808a7bad9
Chamod Shehanka <hcsperera@outlook.com>
2020-04-06 21:01:55 +0530

    Client CRUD controller impled and category module created

--- backend/src/api/client/client.controller.ts
+++ backend/src/api/client/client.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import * as mongodb from "mongodb";
 import { MongoHelper } from "../../config/mongodb.config";
 
 const getCollection = () => {
@@ -7,16 +8,68 @@ const getCollection = () => {
 
 export default class ClientController {
   // private getCollection: MongoHelper.client.db('').collection();
+  // private collection : any = MongoHelper.client.db("ShopDB").collection("clients");
 
   /**
    * Add Client
+   * @param clientID id of the client
    * @param name name of the client
    * @param email client email address
    * @param contactNo client contactNo
    * @returns success or failure message
    */
   public addClient = async (req: Request, res: Response): Promise<any> => {
-    console.log(req.body);
+    const { clientID, name, email, contactNo } = req.body;
+    const collection: any = getCollection();
+
+    collection.insert({
+      _id: clientID,
+      name: name,
+      email: email,
+      contactNo: contactNo,
+    });
+
+    res.send({ message: "Successfully Added" });
+    res.end();
+  };
+
+  /**
+   * Update Client
+   * @param clientID id of the client
+   * @param name name of the client
+   * @param email client email address
+   * @param contactNo client contactNo
+   * @returns success or failure message
+   */
+  public updateClient = async (req: Request, res: Response): Promise<any> => {
+    const { clientID, name, email, contactNo } = req.body;
+    const collection: any = getCollection();
+
+    collection.findOneAndUpdate(
+      {
+        _id: new mongodb.ObjectId(clientID),
+      },
+      {
+        $set: {
+          name: name,
+          email: email,
+          contactNo: contactNo,
+        },
+      }
+    );
+
+    res.send({ message: "Succesfully Updated" });
+    res.end();
+  };
+
+  /**
+   * @param clientID id of the client
+   * @returns success or error message
+   */
+  public deleteClient = async (req: Request, res: Response): Promise<any> => {
+    const collection: any = getCollection();
+    const { clientID } = req.body;
+    collection.remove({ _id: new mongodb.ObjectId(clientID) });
   };
 
   /**
@@ -31,7 +84,7 @@ export default class ClientController {
         res.end();
         console.error("Caught error", err);
       } else {
-        items = items.map(item => {
+        items = items.map((item) => {
           return { id: item._id, description: item.description };
         });
         res.json(items);
