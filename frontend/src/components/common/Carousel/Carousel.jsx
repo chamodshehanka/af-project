9c182be82ec93f19df73a1ff41c5170124de325b
Chamod Shehanka <hcsperera@outlook.com>
2020-05-09 02:59:45 +0530

    update

--- frontend/src/components/common/Carousel/Carousel.jsx
+++ frontend/src/components/common/Carousel/Carousel.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
-import OwlCarousel from "react-owl-carousel";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
-import "./Carousel.css";
+import React, { Component } from 'react';
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import './Carousel.css';
 
 class Carousel extends Component {
   state = {};
   render() {
     return (
       <div>
-        <div className="container-fluid">
+        <div className='container-fluid'>
           <OwlCarousel
             items={1}
-            className="owl-theme"
+            className='owl-theme'
             loop
             nav
             margin={4}
@@ -21,48 +21,41 @@ class Carousel extends Component {
           >
             <div>
               <img
-                className="img"
+                className='img'
                 src={
-                  "https://s3.ap-south-1.amazonaws.com/www.kellyfelder.com/banners/4f587420069060f35438cdb84b8e11273b054d6d.jpg"
+                  'https://s3.ap-south-1.amazonaws.com/www.kellyfelder.com/banners/4f587420069060f35438cdb84b8e11273b054d6d.jpg'
                 }
-
-                alt=""
-
+                alt=''
               />
             </div>
 
             <div>
               <img
-                className="img"
+                className='img'
                 src={
-                  "https://s3.ap-south-1.amazonaws.com/www.kellyfelder.com/banners/7ac0e3b09c981296236a27e92e613be8c98bb6aa.jpg"
+                  'https://s3.ap-south-1.amazonaws.com/www.kellyfelder.com/banners/7ac0e3b09c981296236a27e92e613be8c98bb6aa.jpg'
                 }
-
-                alt=""
-
+                alt=''
               />
             </div>
 
             <div>
               <img
-                className="img"
+                className='img'
                 src={
-                  "https://s3.ap-south-1.amazonaws.com/www.kellyfelder.com/banners/49d4b9979f0f8e8feee32a595796156abe64ee96.png"
+                  'https://s3.ap-south-1.amazonaws.com/www.kellyfelder.com/banners/49d4b9979f0f8e8feee32a595796156abe64ee96.png'
                 }
-
-                alt=""
-
+                alt=''
               />
             </div>
 
             <div>
               <img
-                className="img"
+                className='img'
                 src={
-                  "https://s3.ap-south-1.amazonaws.com/www.kellyfelder.com/banners/e242fbef31680c194bf85ee2be46bb7824d02e0f.jpg"
+                  'https://s3.ap-south-1.amazonaws.com/www.kellyfelder.com/banners/e242fbef31680c194bf85ee2be46bb7824d02e0f.jpg'
                 }
-                alt=""
-
+                alt=''
               />
             </div>
           </OwlCarousel>
