fba7aa27b6f437909891bc7a423c2d2a2d6ce72e
Chamod Shehanka <hcsperera@outlook.com>
2020-05-11 03:45:23 +0530

    minor update (#26)

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,12 +1,14 @@
-import { Router } from "express";
-import client from "./client/client.route";
+import { Router } from 'express';
+import client from './client/client.route';
+import product from './product/product.route';
 
 const router: Router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Response from NodeTS Server");
+router.get('/', (req, res) => {
+  res.send('Response from NodeTS Server');
 });
 
-router.use("/client", client);
+router.use('/client', client);
+router.use('/product', product);
 
 export default router;
