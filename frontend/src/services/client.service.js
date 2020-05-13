f75d0e99c3f10a0a0be0f038e7d89fae5e544356
Chamod Shehanka <hcsperera@outlook.com>
2020-05-13 16:22:04 +0530

    docker file update

--- frontend/src/services/client.service.js
+++ frontend/src/services/client.service.js
@@ -1,5 +1,7 @@
 import Axios from 'axios';
 
+// Create new client
+// TODO: Need add Client object as a param
 export function createNewClient() {
   Axios.post('http://localhost:4000/api/client/add')
     .then((e) => {
@@ -8,12 +10,16 @@ export function createNewClient() {
     .catch((err) => console.error(err));
 }
 
+// Need to impl
 export function removeClient(id) {}
 
+// Need to impl
 export function updateClient() {}
 
+// Need to impl
 export function getClientById(id) {}
 
+// Get all clients
 export function getAllClients() {
   Axios.get('http://localhost:4000/api/client/list')
     .then((e) => {
@@ -21,5 +27,5 @@ export function getAllClients() {
     })
     .catch((err) => console.error(err));
 
-  return 'clients innawa';
+  return 'clientsla innawa';
 }
