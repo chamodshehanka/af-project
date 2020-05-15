fbbdf8f29252a822fba1e8827fcb41a31e347294
Chamod Shehanka <hcsperera@outlook.com>
2020-05-15 16:17:15 +0530

    error fixed

--- frontend/src/routes/appRoutes.jsx
+++ frontend/src/routes/appRoutes.jsx
@@ -4,8 +4,7 @@ import SignUp from '../pages/SignUp/SignUp';
 import SignIn from '../pages/SignIn/SignIn';
 import ClientPage from '../pages/Client/ClientPage';
 import ClientAccount from '../pages/Client/ClientAccount';
-import Dashboard from'../pages/Admin/Dashboard';
-
+import Dashboard from '../pages/Admin/Dashboard';
 
 export const routes = [
   { path: '/', component: HomePage },
@@ -14,6 +13,5 @@ export const routes = [
   { path: '/cart', component: CartPage },
   { path: '/clients', component: ClientPage },
   { path: '/clientAccount', component: ClientAccount },
-  {path:'/Dashboard', component: Dashboard},
-
+  { path: '/Dashboard', component: Dashboard },
 ];
