3c8a91e219880cb1cd219cfdfc7d578cb48bfaf2
Randud <randu612@gmail.com>
2020-05-19 00:17:38 +0530

    unused import issue fixed

--- frontend/src/components/admin/StoreManager.jsx
+++ frontend/src/components/admin/StoreManager.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
