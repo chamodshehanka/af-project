9a1fe48c22e579e4bf77bd8ee85a3fd0141e674d
Randud <randu612@gmail.com>
2020-05-20 01:00:11 +0530

    Store manager details view added

---
+++ frontend/src/services/admin.service.js
@@ -0,0 +1,17 @@
+import Axios from 'axios';
+
+  export function getStoreManager() {
+    var array;
+  
+    Axios.get('http://localhost:4000/api/storeManager/list')
+      .then((e) => {
+        console.log(e.data);
+  
+        array = e.data;
+  
+        return e.data;
+      })
+      .catch((err) => console.error(err));
+  
+    return array;
+  }
--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,5 +1,6 @@
 import * as ClientService from './client.service';
 import * as ProductService from './product.service';
 import * as CategoryService from './category.service';
+import * as AdminService from './admin.service';
 
-export { ClientService, ProductService, CategoryService };
+export { ClientService, ProductService, CategoryService, AdminService};
