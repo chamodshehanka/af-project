bd5d285898ec874a8977dafc2565bdb7fca8ee34
Chamod Shehanka <hcsperera@outlook.com>
2020-05-24 15:00:55 +0530

    Conflict resolved

--- frontend/src/components/storemanager/EditProduct.jsx
+++ frontend/src/components/storemanager/EditProduct.jsx
@@ -5,7 +5,7 @@ import TextField from '@material-ui/core/TextField';
 import { Button } from '@material-ui/core';
 import { useForm } from 'react-hook-form';
 import Paper from '@material-ui/core/Paper';
-import { ProductService } from '../../services'
+import { ProductService } from '../../services';
 //ImageGallery
 
 const categories = [
@@ -20,15 +20,14 @@ const categories = [
 ];
 
 export default function EditProduct() {
-  
   const [category, setCategory] = React.useState('');
   const [productId, setProductId] = React.useState('');
   const [productData, setProductData] = React.useState([]);
 
   const handleSearch = () => {
-    setProductData(ProductService.getProductById(productId)) 
-    console.log(productId)
-    console.log(productData)
+    setProductData(ProductService.getProductById(productId));
+    console.log(productId);
+    console.log(productData);
     // setValue([
     //   { name: productData.name, },
     //   { category: productData.category, },
@@ -36,11 +35,10 @@ export default function EditProduct() {
     //   { stocks: productData.stocks, },
     //   { price: productData.price, },
     // ]);
-    
   };
 
   const handleSelectChange = (event) => {
-    setCategory(event.target.value)
+    setCategory(event.target.value);
   };
 
   const onSubmit = (data) => {
@@ -49,78 +47,121 @@ export default function EditProduct() {
   };
 
   const { register, handleSubmit, setValue, reset } = useForm();
- 
+
   return (
     <div>
       <Grid xs={8}>
-        <Paper style={{height:'650'}}>
-        <Grid item xs={12}style={{marginLeft:'30px', marginRight:'30px', padding:'10px'}}>
-            <Typography variant="h5"  align="left" color="textSecondary" gutterBottom>Edit Product Details</Typography>
-        </Grid>
-        <form noValidate onSubmit={handleSubmit(onSubmit)}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} style={{marginLeft:'30px', marginRight:'30px'}}>
+        <Paper style={{ height: '650' }}>
+          <Grid
+            item
+            xs={12}
+            style={{ marginLeft: '30px', marginRight: '30px', padding: '10px' }}
+          >
+            <Typography
+              variant="h5"
+              align="left"
+              color="textSecondary"
+              gutterBottom
+            >
+              Edit Product Details
+            </Typography>
+          </Grid>
+          <form noValidate onSubmit={handleSubmit(onSubmit)}>
+            <Grid container spacing={3}>
+              <Grid
+                item
+                xs={12}
+                style={{ marginLeft: '30px', marginRight: '30px' }}
+              >
                 <TextField
                   id="searchProduct"
                   name="searchProduct"
                   label="Search Product"
                   fullWidth
-                  onChange={ (e) => {
-                    setProductId(e.target.value)
+                  onChange={(e) => {
+                    setProductId(e.target.value);
                   }}
                   variant="outlined"
                 />
-            </Grid>
-            <Grid item xs={12} align="left" style={{marginLeft:'30px', marginRight:'30px'}}>
-              <Button onClick={handleSearch} variant="outlined" color="default">Search</Button>
-            </Grid>
-            <Grid item xs={12} style={{marginLeft:'30px', marginRight:'30px'}}>
-              <TextField
-                required
-                id="productName"
-                name="name"
-                label="Product Name"
-                fullWidth
-                autoComplete="pname"
-                inputRef={register}
-                variant="outlined"
-              />
               </Grid>
-             <Grid item xs={12} style={{marginLeft:'30px', marginRight:'30px'}}>
+              <Grid
+                item
+                xs={12}
+                align="left"
+                style={{ marginLeft: '30px', marginRight: '30px' }}
+              >
+                <Button
+                  onClick={handleSearch}
+                  variant="outlined"
+                  color="default"
+                >
+                  Search
+                </Button>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                style={{ marginLeft: '30px', marginRight: '30px' }}
+              >
                 <TextField
-                   id="filled-select-currency-native"
-                   name="category"
-                   select
-                   label="Select Category"
-                   fullWidth
-                   value={category}
-                   onChange={handleSelectChange}
-                   inputRef={register}
-                   SelectProps={{
-                     native: true,
-                   }}
-                   variant="outlined"
-                 >
-                   {categories.map((option) => (
-                     <option key={option.value} value={option.value}>
-                     {option.label}
-                   </option>
-                    ))}>
+                  required
+                  id="productName"
+                  name="name"
+                  label="Product Name"
+                  fullWidth
+                  autoComplete="pname"
+                  inputRef={register}
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                style={{ marginLeft: '30px', marginRight: '30px' }}
+              >
+                <TextField
+                  id="filled-select-currency-native"
+                  name="category"
+                  select
+                  label="Select Category"
+                  fullWidth
+                  value={category}
+                  onChange={handleSelectChange}
+                  inputRef={register}
+                  SelectProps={{
+                    native: true,
+                  }}
+                  variant="outlined"
+                >
+                  {categories.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                  >
                 </TextField>
               </Grid>
-              <Grid item xs={12} style={{marginLeft:'30px', marginRight:'30px'}}>
-              <TextField
-                   id="filled-multiline-flexible"
-                   name="description"
-                   label="Description"
-                   multiline
-                   rows={4}
-                   fullWidth
-                   inputRef={register}
-                   variant="outlined"
-               />
+              <Grid
+                item
+                xs={12}
+                style={{ marginLeft: '30px', marginRight: '30px' }}
+              >
+                <TextField
+                  id="filled-multiline-flexible"
+                  name="description"
+                  label="Description"
+                  multiline
+                  rows={4}
+                  fullWidth
+                  inputRef={register}
+                  variant="outlined"
+                />
               </Grid>
-              <Grid item xs={12} style={{marginLeft:'30px', marginRight:'30px'}}>
+              <Grid
+                item
+                xs={12}
+                style={{ marginLeft: '30px', marginRight: '30px' }}
+              >
                 <TextField
                   required
                   id="stock"
@@ -131,7 +172,11 @@ export default function EditProduct() {
                   variant="outlined"
                 />
               </Grid>
-              <Grid item xs={12} style={{marginLeft:'30px', marginRight:'30px'}}>
+              <Grid
+                item
+                xs={12}
+                style={{ marginLeft: '30px', marginRight: '30px' }}
+              >
                 <TextField
                   required
                   id="price"
@@ -142,30 +187,50 @@ export default function EditProduct() {
                   variant="outlined"
                 />
               </Grid>
-              <Grid item xs={12}style={{marginLeft:'30px', marginRight:'30px'}}>
-              <Typography variant="body1" align="left" color="textSecondary" gutterBottom>Images</Typography>
-              <Button variant="outlined" fullWidth color="default">Add Images</Button>
-              </Grid> 
+              <Grid
+                item
+                xs={12}
+                style={{ marginLeft: '30px', marginRight: '30px' }}
+              >
+                <Typography
+                  variant="body1"
+                  align="left"
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  Images
+                </Typography>
+                <Button variant="outlined" fullWidth color="default">
+                  Add Images
+                </Button>
+              </Grid>
 
               <Grid item xs={12} sm={6}>
-                  <Button onClick={() => {
+                <Button
+                  onClick={() => {
                     reset({
-                      name: "",
-                      description: "",
-                      stocks: "",
-                      price: ""
-                    },);}} 
-                    variant="outlined" 
-                    color="default">Clear</Button>
-               </Grid> 
+                      name: '',
+                      description: '',
+                      stocks: '',
+                      price: '',
+                    });
+                  }}
+                  variant="outlined"
+                  color="default"
+                >
+                  Clear
+                </Button>
+              </Grid>
 
-              <Grid item xs={12} sm={6} >
-              <Button type="submit" variant="outlined" color="secondary">Save Changes</Button>
-              </Grid>  
+              <Grid item xs={12} sm={6}>
+                <Button type="submit" variant="outlined" color="secondary">
+                  Save Changes
+                </Button>
+              </Grid>
             </Grid>
           </form>
         </Paper>
-      </Grid>  
+      </Grid>
     </div>
-    );
-  }
+  );
+}
